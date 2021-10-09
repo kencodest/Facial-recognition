e1e46b8a72ade81f9f7553e1f3c08a51c91055e7
kencodest <muiaken88@gmail.com>
2021-10-09 21:52:06 +0300

    modified theme

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[theme]
+primaryColor = '#FF4B4B'
+backgroundColor = '#0E1117'
+secondaryBackgroundColor = '#262730'
+textColor = '#FAFAFA'
+font = 'sans serif'
 [server]
 port = $PORT
 enableCORS = false
